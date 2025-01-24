b1e9d16a96877289f26f01ff32eb0baa364afda5
romainB <romain.jimmy.bergout@gmail.com>
2025-01-24 12:31:17 +0100

    Modif 24-01-25

--- Front-End/accueil.js
+++ Front-End/accueil.js
@@ -1,29 +1,32 @@
 const iconBurger = document.querySelector("#icon-burger");
 const iconBasket = document.querySelector("#icon-basket");
 const menuBurger = document.querySelector(".menu-burger");
-
-let classes = menuBurger.classList;
-menuBurger.style.display = classes.value;
-
 const basket = document.querySelector(".basket");
+const overlay = document.querySelector(".overlay");
 
-iconBurger.addEventListener("click", function(){
- 
-  console.log("J'ai Clické sur le menu burger");
-  changeBlock(menuBurger);
-});
 
-iconBasket.addEventListener("click", function(){
-  
-  console.log("J'ai Clické sur le menu panier");
-  changeBlock(basket);
-});
+function changeIcon(icon, typeIcon){
+  // console.log(icon);
+  // if(icon.src == `/Images/Icon-Menu-${typeIcon}.png`)
+  // {
+  //   icon.src = "/Images/Icon-Menu-Close.png";
+  // }
+  // else if(icon.src == "*/Images/Icon-Menu-Close.png"){
+  //   icon.src = `/Images/Icon-Menu-${typeIcon}.png`;
+  // }
 
-function changeBlock(elementChange) {
-  // elementChange.style.display = elementChange.style.display == "none" ? "block" : "none"; //Ternaire du click declick pseudo toggle.
-  if (elementChange.style.display == "none") {
-    elementChange.style.display = "block";
-  } else {
-    elementChange.style.display = "none";
-  }
+  icon.src = icon.src.includes(`/Images/Icon-Menu-${typeIcon}.png`) 
+  ? `/Images/Icon-Menu-Close.png` 
+  : `/Images/Icon-Menu-${typeIcon}.png`;
+
 }
+
+iconBurger.addEventListener('click', function() {
+  changeIcon(iconBurger, "Burger");
+  menuBurger.classList.toggle('active');
+});
+
+iconBasket.addEventListener('click', function() {
+  changeIcon(iconBasket, "Basket");
+  basket.classList.toggle('active');
+});
