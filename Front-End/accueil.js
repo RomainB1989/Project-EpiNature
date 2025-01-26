805f1dbec1d145e3c30852909b03eb93284ee14c
Romain BERGOUT <romain.jimmy.bergout@gmail.com>
2025-01-26 16:56:39 +0100

    Modif 26-01-2025

--- Front-End/accueil.js
+++ Front-End/accueil.js
@@ -13,8 +13,9 @@ function changeIcon(icon, typeIcon){
   // }
   // else if(icon.src == "*/Images/Icon-Menu-Close.png"){
   //   icon.src = `/Images/Icon-Menu-${typeIcon}.png`;
-  // }
+  //
 
+  //Ternaire pour transformer l'icone du bouton selectionné.
   icon.src = icon.src.includes(`/Images/Icon-Menu-${typeIcon}.png`) 
   ? `/Images/Icon-Menu-Close.png` 
   : `/Images/Icon-Menu-${typeIcon}.png`;
@@ -22,13 +23,21 @@ function changeIcon(icon, typeIcon){
 }
 
 iconBurger.addEventListener('click', function() {
-  changeIcon(iconBurger, "Burger");
-  // basket.classList.remove('active');
+  if(basket.classList.contains('active'))
+  {
+    basket.classList.remove('active');
+    changeIcon(iconBasket, "Basket");
+  }
   menuBurger.classList.toggle('active');
+  changeIcon(iconBurger, "Burger");
 });
 
 iconBasket.addEventListener('click', function() {
-  changeIcon(iconBasket, "Basket");
-  // menuBurger.classList.remove('active');
-  basket.classList.toggle('active');
+  if(menuBurger.classList.contains('active'))
+    {
+      menuBurger.classList.remove('active');
+      changeIcon(iconBurger, "Burger");
+    }
+    basket.classList.toggle('active');
+    changeIcon(iconBasket, "Basket");
 });
