7edaa9cae1d4c479dd1e42c9a1ba010ce185f05f
Romain BERGOUT <romain.jimmy.bergout@gmail.com>
2025-02-09 18:11:47 +0100

    Modif 09-02-2025

--- Front-End/slideshow.js
+++ Front-End/slideshow.js
@@ -2,7 +2,7 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
+function changeSlides(n) {
   showSlides(slideIndex += n);
 }
 
@@ -15,12 +15,16 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (n > slides.length){
+      slideIndex = 1;
+    }
+  if (n < 1){
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++){
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  for (i = 0; i < dots.length; i++){
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
