088290dd194f4ce066ba662decc6d64d98574ebd
romainB <romain.jimmy.bergout@gmail.com>
2025-02-20 11:24:57 +0100

    Add fichier Insert BDD pour Test produits

---
+++ db/insertProductsDB.sql
@@ -0,0 +1,18 @@
+Use epinature;
+INSERT INTO products (name_product, resume, description, ingredients, price, stock_number, is_available, category_id) VALUES
+('Baguette Tradition', 'Baguette croustillante à l''ancienne', 'Une baguette traditionnelle à la croûte dorée et croustillante avec une mie aérée.', 'Farine de blé, eau, levure, sel', 1.20, 50, 1, 1),
+('Pain aux Céréales', 'Pain riche en fibres et graines', 'Pain complet aux multiples céréales pour un petit-déjeuner équilibré.', 'Farine complète, graines (lin, tournesol, sésame), eau, levure, sel', 2.50, 30, 1, 1),
+('Croissant au Beurre', 'Viennoiserie feuilletée au beurre', 'Croissant pur beurre, croustillant à l''extérieur et moelleux à l''intérieur.', 'Farine de blé, beurre, lait, sucre, levure, sel', 1.10, 40, 1, 2),
+('Pain au Chocolat', 'Viennoiserie fourrée au chocolat', 'Délicieuse viennoiserie feuilletée garnie de deux barres de chocolat noir.', 'Farine de blé, beurre, chocolat noir, lait, sucre, levure, sel', 1.20, 35, 1, 2),
+('Tarte aux Pommes', 'Tarte aux pommes fraîches', 'Tarte garnie de pommes fraîches sur une délicieuse crème pâtissière.', 'Pâte brisée, pommes, crème pâtissière, sucre, beurre', 12.50, 10, 1, 3),
+('Éclair au Café', 'Pâtisserie fourrée à la crème au café', 'Pâte à choux croustillante garnie d''une onctueuse crème au café.', 'Farine, œufs, beurre, lait, café, sucre', 2.80, 20, 1, 3),
+('Pain de Campagne', 'Pain rustique à la mie dense', 'Pain de tradition française à la mie dense et au goût prononcé.', 'Farine de blé, farine de seigle, eau, levain, sel', 3.00, 25, 1, 1),
+('Brioche Tressée', 'Brioche moelleuse au beurre', 'Brioche dorée et moelleuse, parfaite pour le petit-déjeuner ou le goûter.', 'Farine de blé, beurre, œufs, sucre, lait, levure', 4.50, 15, 1, 2),
+('Mille-feuille', 'Pâtisserie feuilletée à la vanille', 'Trois couches de pâte feuilletée garnies de crème pâtissière à la vanille.', 'Pâte feuilletée, crème pâtissière, sucre glace, vanille', 3.50, 12, 1, 3),
+('Baguette aux Olives', 'Baguette aromatisée aux olives', 'Baguette savoureuse parsemée d''olives noires pour un goût méditerranéen.', 'Farine de blé, eau, olives noires, levure, sel', 2.00, 20, 1, 1),
+('Chausson aux Pommes', 'Chausson fourré aux pommes', 'Pâte feuilletée dorée renfermant une délicieuse compote de pommes.', 'Pâte feuilletée, pommes, sucre, cannelle', 2.30, 25, 1, 2),
+('Tarte au Citron Meringuée', 'Tarte acidulée et sucrée', 'Fond de tarte garni de crème au citron et recouvert d''une meringue légère.', 'Pâte sucrée, citron, œufs, sucre, beurre', 14.00, 8, 1, 3),
+('Pain de Seigle', 'Pain dense au seigle', 'Pain au seigle à la mie serrée et au goût caractéristique.', 'Farine de seigle, eau, levain, sel', 2.80, 18, 1, 1),
+('Pain au Lait', 'Petit pain moelleux au lait', 'Petit pain brioché au lait, idéal pour le goûter des enfants.', 'Farine de blé, lait, beurre, sucre, levure', 0.80, 40, 1, 2),
+('Opéra', 'Gâteau multicouche au café et chocolat', 'Délicate alternance de biscuit Joconde, crème au beurre café et ganache chocolat.', 'Biscuit amande, café, chocolat noir, crème au beurre, sirop de café', 4.00, 10, 1, 3);
+
